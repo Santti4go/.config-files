6605970d2efbc77e83a60ac87cbf6c626fdd5fa4
Santiago <santiaupi@gmail.com>
2024-01-05 11:25:51 -0300

    add black theme

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -14,20 +14,25 @@ while pgrep -x "polybar" >/dev/null; do sleep 1; done
 for monit in $(xrandr --query | grep " connected" | cut -d " " -f1);do
 
 # Left bar
-MONITOR=$monit polybar --reload my-name -c ~/.config/polybar/current.ini &
-MONITOR=$monit polybar --reload date -c ~/.config/polybar/current.ini &
-
-# Spotify
-MONITOR=$monit polybar --reload spotify -c ~/.config/polybar/current.ini &
-
-## Right bar
-MONITOR=$monit polybar --reload top -c ~/.config/polybar/current.ini &
-MONITOR=$monit polybar --reload level -c ~/.config/polybar/current.ini &
-
-# My calendar
-MONITOR=$monit polybar --reload calendar -c ~/.config/polybar/current.ini &
+# MONITOR=$monit polybar --reload my-name -c ~/.config/polybar/current.ini &
+# MONITOR=$monit polybar --reload date -c ~/.config/polybar/current.ini &
 
 ## Center bar
 MONITOR=$monit polybar --reload primary -c ~/.config/polybar/current.ini &
+
+# Spotify
+# MONITOR=$monit polybar --reload spotify -c ~/.config/polybar/current.ini &
+
+MONITOR=$monit polybar -q left-bar -c ~/.config/polybar/current.ini &
+
+## Right bar
+# MONITOR=$monit polybar --reload top -c ~/.config/polybar/current.ini &
+MONITOR=$monit polybar -q right-bar -c ~/.config/polybar/current.ini &
+
+# MONITOR=$monit polybar --reload level -c ~/.config/polybar/current.ini &
+
+# # My calendar
+# MONITOR=$monit polybar --reload calendar -c ~/.config/polybar/current.ini &
+
 
 done
